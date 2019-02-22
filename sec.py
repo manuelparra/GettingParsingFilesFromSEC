104354def420d2c703536b9f67869d0d6be3d052
Manuel Parra <manuelparra@live.com.ar>
2019-02-21 23:54:25 -0400

    v1.0

--- sec.py
+++ sec.py
@@ -4,9 +4,8 @@
 """
 @description: script for get and parse files of the SEC website
 @author: Manuel Parra
-@date:
+@date: 21/02/19
 @licence:
-
 The MIT License
 
 Copyright (c) 2019
@@ -28,13 +27,25 @@ AUTHORS OR COPYRIGHT HOLDER BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABLILITY, WHETHER IN AN ACTION OF CONTRAC, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
-
 """
+
 from bs4 import BeautifulSoup
 import urllib.request, urllib.parse, urllib.error
+import nettest
+import time
+import sys
 import csv
 import ssl
-import time
+
+# checking the Internet connection
+print("Testing your Internet connection, please wait!")
+host = ['8.8.8.8', '8.8.4.4', '1.1.1.1']
+nt = nettest.chargetest(host)
+if not nt.isnetup():
+    print("Your Internet connection  is down!, please try again latter")
+    sys.exit()
+
+print("Your Internet connection is fine!")
 
 # get year
 ts = time.time() # time in seconds
@@ -46,8 +57,8 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
+print("Retrieving the paths to get the files, please wait a moment!")
 url = "https://www.sec.gov/Archives/edgar/full-index/"
-
 html = urllib.request.urlopen(url, context=ctx).read()
 soup = BeautifulSoup(html, 'html.parser')
 table = soup.find('table')
@@ -67,3 +78,35 @@ for url in links:
         qtrs.append(url + row.get('href', None))
 
     if (url[-5:-1] == str(year)): break
+
+print("We are begining to get data files")
+
+for url in qtrs:
+    url += 'master.idx'
+    print("Retrieve", url)
+    fhand = urllib.request.urlopen(url, context=ctx)
+    findtitle = False
+    data = []
+    for line in fhand:
+        text = line.decode('utf-8')
+        lt = text.strip().split('|')
+        if not findtitle and lt[0] != 'CIK': continue
+        if len(lt) < 2: continue
+        findtitle = True
+        CIK = lt[0]
+        COMPANYNAME = lt[1]
+        FORMTYPE = lt[2]
+        DATEFILED = lt[3]
+        FILENAME = lt[4]
+        ROW = [CIK, COMPANYNAME, FORMTYPE, DATEFILED, FILENAME]
+        data.append(ROW)
+
+    fname = url[-20:-11]
+    fname = fname.replace('/', '_')
+    fname += '.csv'
+    print("Write a file", fname)
+    with open('files/'+fname, 'w') as file:
+        write = csv.writer(file)
+        write.writerows(data)
+
+print("Done!")
